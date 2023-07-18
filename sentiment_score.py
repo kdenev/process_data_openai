906c58e61c0e5002b67c687826a19576b6056fef
kdenev <kristiyan.denev@gmail.com>
2023-07-18 17:36:10 +0200

    remove openai credentials

--- sentiment_score.py
+++ sentiment_score.py
@@ -6,9 +6,14 @@ from datetime import datetime
 import json
 import time
 
+# Polars options
 pl.Config.set_tbl_rows(50)
 pl.Config.set_tbl_cols(20)
 pl.Config.set_fmt_str_lengths(100)
+
+# Openai credentials
+os.environ['OPENAI_API_KEY'] = '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'
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Import data
 df = pl.read_csv('gdelt_api\output20230707.csv')
@@ -31,7 +36,7 @@ list_duped_names = (df
 df_deduped = df.with_columns(pl.col('title').str.to_lowercase()).groupby('title').first()
 
 # Test Sample
-
+# Covers 1 whole week
 sample_news = (df_deduped
                 .with_columns([
                     pl.col('seendate')
@@ -53,10 +58,6 @@ sample_news = (df_deduped
 # Sample
 sample_news.groupby(['date']).count().sort('date', descending=True)
 sample_news.groupby(['weekday']).count().sort('weekday')
-
-# Openai credentials
-openai.organization = "org-WUwBe3JxtbS5s9X8zhF4DY3K"
-openai.api_key = "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"
 
 # Output dataframe
 openai_response = pl.DataFrame()
@@ -90,25 +91,3 @@ for i in sample_news['row_nr']:
   openai_response = openai_response.vstack(loop_df)
 
 output = sample_news.join(openai_response, how='inner', left_on='row_nr', right_on='id')
-
-
-  # response = openai.Completion.create(
-  #   model="text-davinci-003",
-  #   prompt=f"""Data id: {sample_news['row_nr'][i]}
-  #               Title: {sample_news['title'][i]}
-  #               Return the id number
-  #               , sentiment score of the title (Sentiment score can be postive negative or neutral.)
-  #               , the most import word for the sentiment
-  #               , the country it relates to(if not available use "Other")
-  #               , the region it relates - options are US, China, UK, Americas, Asia, Europe, Africa, Other
-  #               and the tone of the title is in range from 1 to -1 with intervals of .1(always return a float).
-  #               Data format of the response is a dictionary. 
-  #               Keys values are -  "id", "sentiment", "word", "country", "region", "tone".
-  #               Return should do not contain any other characters other than the dictionary!
-  #               String values should be writen with double quotes!
-  #               """,
-  #     max_tokens=100,
-  #     temperature=0
-  # )
-
-  # response['choices'][0]['text'].strip()
